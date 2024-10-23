732a414f3d44be953af71232abb0926c3b6d14ab
Seizuma <stanleguen.pro@gmail.com>
2024-10-23 14:31:19 +0200

    ADD: Ajout d'une photo et mise à jour du positionnement

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,18 +3,39 @@ import styled from 'styled-components';
 
 const ProfileContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+//   justify-content: space-between;
   align-items: center;
-  padding: 100px 20px;
+//   padding: 10px 10px;
   background: #000;
-  color: #f5f5dc;
-  min-height: 100vh;
+  min-height: 50vh;
 `;
 
 const ProfileText = styled.div`
   width: 50%;
-  margin-left: 10%; /* Ajuste cette valeur pour aligner le texte à "Stan" */
-  font-size: 1.2rem;
+  margin-left: 10%;
+  margin-top: -20%;
+`;
+
+const Heading = styled.h2`
+  font-size: 2rem;
+  color: #daa520; 
+`;
+
+const Name = styled.p`
+  font-size: 4rem;
+  font-weight: bold;
+//   color: #f5f5dc;
+  margin: 10px 0;
+
+
+  & > span {
+    color: #daa520; 
+  }
+`;
+
+const Role = styled.p`
+  font-size: 2.5rem;
+//   color: #f5f5dc;
 `;
 
 const ProfilePhoto = styled.div`
@@ -22,11 +43,12 @@ const ProfilePhoto = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: -5%;
 `;
 
 const Photo = styled.img`
   width: 100%;
-  max-width: 400px;
+  max-width: 1000px;
   border-radius: 8px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
 `;
@@ -35,12 +57,15 @@ const Profile = () => {
   return (
     <ProfileContainer>
       <ProfileText>
-        <h2>À propos de moi</h2>
-        <p>Je suis un développeur passionné avec une expérience en développement web et des compétences variées. Mon objectif est de continuer à améliorer mes compétences et de contribuer à des projets intéressants.</p>
+        <Heading>Bienvenue !</Heading>
+        <Name>
+          Je suis <span>Stan Le Guen</span>
+        </Name>
+        <Role>Développeur Full Stack</Role>
       </ProfileText>
       <ProfilePhoto>
         {/* Ajoute ta photo ici */}
-        <Photo src="chemin/vers/ta/photo.jpg" alt="Photo de moi" />
+        <Photo src="../../pictures/Pic1.png" alt="Photo de moi" />
       </ProfilePhoto>
     </ProfileContainer>
   );
