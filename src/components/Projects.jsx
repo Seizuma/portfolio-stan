ac401b9ac7db4ad574d6b2179b0a0903dce70965
Seizuma <stanleguen.pro@gmail.com>
2024-10-24 11:42:31 +0200

    WIP: ajout de projets et animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { FaCode, FaTasks, FaShoppingCart } from 'react-icons/fa'; // Icônes pour les projets
+import { FaProjectDiagram, FaMicrophoneAlt } from 'react-icons/fa';
 
 const ProjectsContainer = styled.div`
   text-align: center;
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  justify-content: center; /* Centrer verticalement */
+  justify-content: center;
 `;
 
 const ProjectGrid = styled.div`
@@ -16,15 +16,18 @@ const ProjectGrid = styled.div`
   gap: 20px;
 `;
 
-const ProjectCard = styled.div`
+const ProjectCardStyled = styled.div`
   background: #1a1a1a;
-  padding: 20px;
+  padding: ${(props) => (props.$isExpanded ? '20px' : '10px')};  /* Utilisation de $isExpanded */
   border-radius: 8px;
-  transition: transform 0.3s ease;
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  cursor: pointer;
+  overflow: hidden;
+  transition: transform 0.3s ease, max-height 0.5s ease-in-out, padding 0.5s ease-in-out;
+  max-height: ${(props) => (props.$isExpanded ? '500px' : '150px')}; /* Utilise $isExpanded */
+  
   &:hover {
     transform: scale(1.05);
   }
@@ -41,28 +44,84 @@ const ProjectTitle = styled.h3`
 
 const ProjectHeader = styled.h2`
   font-size: 50px;
-`
+`;
+
+const ProjectDescription = styled.p`
+  margin-top: 15px;
+  color: #e8e6e3;
+`;
+
+const ProjectDetails = styled.div`
+  margin-top: 10px;
+  text-align: left;
+  color: #e8e6e3;
+`;
+
+const Technologies = styled.p`
+  font-weight: bold;
+  color: #daa520;
+`;
+
+const ProjectCard = ({ isExpanded, onClick, icon, title, description, details }) => (
+  <ProjectCardStyled $isExpanded={isExpanded} onClick={onClick}>
+    <ProjectIcon>{icon}</ProjectIcon>
+    <ProjectTitle>{title}</ProjectTitle>
+    {isExpanded && (
+      <>
+        <ProjectDescription>{description}</ProjectDescription>
+        {details && (
+          <ProjectDetails>{details}</ProjectDetails>
+        )}
+      </>
+    )}
+  </ProjectCardStyled>
+);
 
 const Projects = () => {
+  const [expandedProject, setExpandedProject] = useState(null);
+
+  const toggleExpand = (project) => {
+    setExpandedProject(expandedProject === project ? null : project);
+  };
+
   return (
     <ProjectsContainer className="fade-in">
-        <ProjectHeader><h2>Projets</h2></ProjectHeader>
+      <ProjectHeader>Projets</ProjectHeader>
       <ProjectGrid>
-        <ProjectCard>
-          <ProjectIcon><FaCode /></ProjectIcon>
-          <ProjectTitle>Projet de Portfolio</ProjectTitle>
-          <p>Un site web personnel pour présenter mes compétences et mes projets.</p>
-        </ProjectCard>
-        <ProjectCard>
-          <ProjectIcon><FaTasks /></ProjectIcon>
-          <ProjectTitle>Application de Gestion de Tâches</ProjectTitle>
-          <p>Une application pour organiser et gérer des tâches quotidiennes.</p>
-        </ProjectCard>
-        <ProjectCard>
-          <ProjectIcon><FaShoppingCart /></ProjectIcon>
-          <ProjectTitle>Application E-commerce</ProjectTitle>
-          <p>Une plateforme de vente en ligne avec gestion des commandes et des utilisateurs.</p>
-        </ProjectCard>
+
+                {/* LearningBeatbox2.0 Project */}
+                <ProjectCard
+          isExpanded={expandedProject === 'learningbeatbox'}
+          onClick={() => toggleExpand('learningbeatbox')}
+          icon={<FaMicrophoneAlt />}
+          title="LearningBeatbox 2.0"
+          description="Application permettant un meilleur apprentissage du beatbox via Machine Learning. Elle analyse la structure, les dynamiques et le genre musical pour aider à corriger les erreurs."
+          details={
+            <>
+              <p>Basé sur le projet Parrot.py</p>
+              <p><strong>Nombre de personnes sur le projet:</strong> 1</p>
+              <Technologies>Technologies: Python, Electron</Technologies>
+            </>
+          }
+        />
+
+
+        {/* ResRel Project */}
+        <ProjectCard
+          isExpanded={expandedProject === 'resrel'}
+          onClick={() => toggleExpand('resrel')}
+          icon={<FaProjectDiagram />}
+          title="ResRel"
+          description="ResRel est un projet visant à améliorer les relations entre les citoyens grâce à une plateforme de partage de ressources (images, PDF, vidéos, événements)."
+          details={
+            <>
+              <p><strong>Nombre de personnes sur le projet:</strong> 4</p>
+              <Technologies>Technologies: React, React Native, PHP, Laravel</Technologies>
+            </>
+          }
+        />
+
+
       </ProjectGrid>
     </ProjectsContainer>
   );
