2ae71fc3a9b4d0b873c656459ce0ce07dc0d5c04
Seizuma <stanleguen.pro@gmail.com>
2024-10-24 10:02:37 +0200

    ADD: ajout animation d'apparition

--- src/index.js
+++ src/index.js
@@ -9,3 +9,22 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeIns = document.querySelectorAll('.fade-in');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.2 // 10% de la section doit être visible pour déclencher l'animation
+  });
+
+  fadeIns.forEach(section => {
+    observer.observe(section);
+  });
+});
