06ba219db3e876a5e6b0855261046252420f34d6
Seizuma <stanleguen.pro@gmail.com>
2024-10-24 10:02:58 +0200

    ADD: ajout des formations

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,23 +7,62 @@ const EducationContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  padding: 50px 20px;
 `;
 
 const EducationItem = styled.div`
   margin: 20px 0;
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  font-size: 2.5rem;
+  margin-bottom: 30px;
+  color: #daa520; /* Couleur or pour le titre */
+  font-weight: bold; /* Mettre le titre en gras */
+`;
+
+const DegreeTitle = styled.h3`
+  font-size: 1.8rem;
+  margin-bottom: 10px;
+  color: #daa520; /* Couleur or pour les titres des diplômes */
+  font-weight: bold; /* Mettre les titres en gras */
+`;
+
+const Institution = styled.p`
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+  color: #fff; /* Texte en blanc */
+`;
+
+const Date = styled.p`
+  font-size: 1rem;
+  color: #fff; /* Texte en blanc */
 `;
 
 const Education = () => {
   return (
-    <EducationContainer>
-      <h2>Éducation</h2>
+    <EducationContainer className="fade-in">
+      <Title>Formation & Diplômes</Title>
+
       <EducationItem>
-        <h3>Master en Informatique</h3>
-        <p>Université XYZ - 2020</p>
+        <DegreeTitle>Bachelor Concepteur Développeur D'applications</DegreeTitle>
+        <Institution>CESI Dijon</Institution>
+        <Date>De septembre 2022 à septembre 2023</Date>
       </EducationItem>
+
       <EducationItem>
-        <h3>Licence en Informatique</h3>
-        <p>Université ABC - 2018</p>
+        <DegreeTitle>BTS Système numérique option Informatique et Réseaux</DegreeTitle>
+        <Institution>Lycée Gustave Eiffel, Dijon, France</Institution>
+        <Date>De septembre 2020 à juillet 2022</Date>
+        <Date>Obtenu mention bien</Date>
+      </EducationItem>
+
+      <EducationItem>
+        <DegreeTitle>Baccalauréat S - Option Sciences de l'Ingénieur</DegreeTitle>
+        <Institution>Lycée Gustave Eiffel, Dijon, France</Institution>
+        <Date>De 2019 à 2020</Date>
       </EducationItem>
     </EducationContainer>
   );
