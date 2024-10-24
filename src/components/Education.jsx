14836ed00cc1b7dd8d161366910a056f13a75481
Seizuma <stanleguen.pro@gmail.com>
2024-10-24 11:42:56 +0200

    ADD: Ajout experience professionelle et redesign

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,70 +1,120 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const EducationContainer = styled.div`
-  text-align: center;
-  flex-grow: 1;
+const SectionContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-around;
+  align-items: flex-start;
   padding: 50px 20px;
+  flex-wrap: wrap;
 `;
 
-const EducationItem = styled.div`
-  margin: 20px 0;
-  text-align: center;
+const Column = styled.div`
+  width: 45%;
+  margin-bottom: 20px;
+
+  @media (max-width: 768px) {
+    width: 100%; /* Sur mobile, les colonnes prennent 100% de largeur */
+  }
 `;
 
-const Title = styled.h2`
+const SectionTitle = styled.h2`
   font-size: 2.5rem;
   margin-bottom: 30px;
-  color: #daa520; /* Couleur or pour le titre */
-  font-weight: bold; /* Mettre le titre en gras */
+  color: #daa520;
+  font-weight: bold;
+  text-align: center;
 `;
 
-const DegreeTitle = styled.h3`
+const Item = styled.div`
+  margin-bottom: 20px;
+  padding: 20px;
+  background: #2a2a2a;
+  border-radius: 10px;
+  text-align: left;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
+`;
+
+const ItemTitle = styled.h3`
   font-size: 1.8rem;
-  margin-bottom: 10px;
-  color: #daa520; /* Couleur or pour les titres des diplômes */
-  font-weight: bold; /* Mettre les titres en gras */
+  margin-bottom: 5px;
+  color: #daa520;
+  font-weight: bold;
 `;
 
 const Institution = styled.p`
   font-size: 1.2rem;
-  margin-bottom: 5px;
-  color: #fff; /* Texte en blanc */
+  color: #fff;
 `;
 
 const Date = styled.p`
   font-size: 1rem;
-  color: #fff; /* Texte en blanc */
+  color: #aaa;
+`;
+
+const Description = styled.p`
+  font-size: 1.2rem;
+  color: #fff;
+  margin-top: 10px;
+`;
+
+const Technologies = styled.p`
+  font-size: 1rem;
+  margin-top: 10px;
+  color: #daa520;
+  font-weight: bold;
 `;
 
 const Education = () => {
   return (
-    <EducationContainer className="fade-in">
-      <Title>Formation & Diplômes</Title>
+    <SectionContainer className="fade-in">
+      
+      {/* Colonne Expérience Professionnelle */}
+      <Column>
+        <SectionTitle>Expérience Professionnelle</SectionTitle>
 
-      <EducationItem>
-        <DegreeTitle>Bachelor Concepteur Développeur D'applications</DegreeTitle>
-        <Institution>CESI Dijon</Institution>
-        <Date>De septembre 2022 à septembre 2023</Date>
-      </EducationItem>
+        <Item>
+          <ItemTitle>Développeur Full Stack</ItemTitle>
+          <Institution>Comeca - 1 an (CDI)</Institution>
+          <Date>2022 - 2023</Date>
+          <Description>Conception et développement de solutions logicielles personnalisées pour répondre aux besoins des clients dans le secteur industriel.</Description>
+          <Technologies>Technologies : Python, C#, WinCC, Windev</Technologies>
+        </Item>   
 
-      <EducationItem>
-        <DegreeTitle>BTS Système numérique option Informatique et Réseaux</DegreeTitle>
-        <Institution>Lycée Gustave Eiffel, Dijon, France</Institution>
-        <Date>De septembre 2020 à juillet 2022</Date>
-        <Date>Obtenu mention bien</Date>
-      </EducationItem>
+        <Item>
+          <ItemTitle>Développeur Full Stack</ItemTitle>
+          <Institution>Comeca - 1 an (Alternance)</Institution>
+          <Date>2021 - 2022</Date>
+          <Description>Développement d'applications web full stack en utilisant des technologies modernes pour divers projets industriels.</Description>
+          <Technologies>Technologies : HTML, CSS, JavaScript, ExpressJs, NodeJs</Technologies>
+        </Item>
 
-      <EducationItem>
-        <DegreeTitle>Baccalauréat S - Option Sciences de l'Ingénieur</DegreeTitle>
-        <Institution>Lycée Gustave Eiffel, Dijon, France</Institution>
-        <Date>De 2019 à 2020</Date>
-      </EducationItem>
-    </EducationContainer>
+      </Column>
+
+      {/* Colonne Formation */}
+      <Column>
+        <SectionTitle>Formation & Diplômes</SectionTitle>
+        
+        <Item>
+          <ItemTitle>Bachelor Concepteur Développeur d'Applications</ItemTitle>
+          <Institution>CESI Dijon</Institution>
+          <Date>De septembre 2022 à septembre 2023</Date>
+        </Item>
+
+        <Item>
+          <ItemTitle>BTS Systèmes Numériques, option Informatique et Réseaux</ItemTitle>
+          <Institution>Lycée Gustave Eiffel, Dijon</Institution>
+          <Date>De septembre 2020 à juillet 2022</Date>
+          <Date>Obtenu mention bien</Date>
+        </Item>
+
+        <Item>
+          <ItemTitle>Baccalauréat Scientifique, Option Sciences de l'Ingénieur</ItemTitle>
+          <Institution>Lycée Gustave Eiffel, Dijon</Institution>
+          <Date>De 2019 à 2020</Date>
+        </Item>
+      </Column>
+    </SectionContainer>
   );
 };
 
